8504af2be8e3719f650a0d34ee65ddb923dd4736
Himanshu Garg <himanshugarg5480@gmail.com>
2024-08-12 00:42:03 +0530

    changes

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv"
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize("mysql://u8ougor1hupljouu:JyRSJMSYmRGSCDssY45m@bdf1wfqf5ac34ak9cgkh-mysql.services.clever-cloud.com:3306/bdf1wfqf5ac34ak9cgkh", {
     host: 'localhost',
     dialect: 'mysql',
 });
